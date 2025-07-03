f7ec5823018f89e55a9315bff044e68dbd667acb
kangourouuu <bmkhanh436@gmail.com>
2025-07-03 09:41:40 +0700

    edit model

--- model/todo.go
+++ model/todo.go
@@ -7,9 +7,10 @@ import (
 )
 
 type Todo struct {
-	ID          int           `json:"id" gorm:"primaryKey;autoIncrement"`
+	ID          int            `json:"id" gorm:"primaryKey;autoIncrement"`
 	Name        string         `json:"name" gorm:"not null"`
 	Description string         `json:"description" gorm:"not null"`
+	Status      string         `json:"status" gorm:"default:doing,not null"`
 	CreatedAt   time.Time      `json:"created_at" gorm:"autoCreateTime"`
 	UpdatedAt   time.Time      `json:"updated_at" gorm:"autoUpdateTime"`
 	DeletedAt   gorm.DeletedAt `json:"deleted_at,omitempty" gorm:"index" swaggerignore:"true"`
